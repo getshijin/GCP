b514b6a11a3291471cf119c06f434c6cb053d07e
Shijin Mohanan Sheela <getshijin@gmail.com>
2026-02-09 15:46:00 +0100

    09/02/26 v2

--- project1/DAG.py
+++ project1/DAG.py
@@ -1,27 +1,34 @@
 from datetime import datetime, timedelta
 from airflow import DAG
-from airflow.operators.bash_operator import BashOperator
-from airflow.utils.dates import days_ago
+from airflow.operators.bash import BashOperator
+from airflow.providers.google.cloud.operators.datafusion import CloudDataFusionStartPipelineOperator
 
 default_args = {
-    'owner': 'airflow',
-    'start_date': datetime(2023, 12, 18),
-    'depends_on_past': False,
-    'email': ['vishal.bulbule@techtrapture.com'],
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
+    "owner": "airflow",
+    "start_date": datetime(2023, 12, 18),
+    "depends_on_past": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
 }
 
-dag = DAG('EMPLOYEE_DATA_PIPELINE',
-          default_args=default_args,
-          description='Runs an external Python script',
-          schedule_interval='@daily',
-          catchup=False)
+with DAG(
+    dag_id="EMPLOYEE_DATA_PIPELINE",
+    default_args=default_args,
+    schedule_interval="@daily",
+    catchup=False,
+) as dag:
 
-with dag:
     run_script_task = BashOperator(
-        task_id='extract_data',
-        bash_command='python /home/airflow/gcs/dags/scripts/extract.py',
+        task_id="extract_data",
+        bash_command="python /home/airflow/gcs/dags/scripts/extract.py",
     )
+
+    start_pipeline = CloudDataFusionStartPipelineOperator(
+        task_id="start_data_fusion_pipeline",
+        location="us-central1",
+        instance_name="datafusion-dev",
+        pipeline_name="first_pipeline",
+        pipeline_timeout=1000,
+    )
+
+    run_script_task >> start_pipeline
